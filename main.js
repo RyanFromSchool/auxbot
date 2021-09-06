5f7f5ff602f5d6fb6de822133ef7141d650046db
Ryan Cabell <cabell.ryan@gmail.com>
2021-09-06 19:38:35 -0400

    bot can go online

---
+++ main.js
@@ -0,0 +1,12 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log('Auxbot is online!');
+});
+
+
+client.login(token);
+
